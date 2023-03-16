cf21f8ce58b819ca70c14b7f54264d3f9bb6ff73
yesseruser <yesseruseryt@gmail.com>
2023-03-16 07:19:10 +0100

    Removed printing and added a textblock to the copy button flyout

--- yNotes/MainPage.xaml.cs
+++ yNotes/MainPage.xaml.cs
@@ -24,6 +24,10 @@ using Windows.UI;
 using Windows.UI.Core;
 using Windows.UI.Xaml.Media.Animation;
 using Windows.ApplicationModel;
+using Windows.Graphics.Printing;
+using Windows.UI.Xaml.Printing;
+using Windows.ApplicationModel.Core;
+using Windows.ApplicationModel.DataTransfer;
 
 // Dokumentaci k šabloně položky Prázdná stránka najdete na adrese https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x405
 
@@ -96,7 +100,7 @@ namespace yNotes
                 notesLB.Items.Remove(item);
             }
 
-            
+
             SaveStuff();
         }
 
@@ -229,6 +233,7 @@ namespace yNotes
             SaveStuff();
         }
 
+        #region Edit
         private void editB_Click(object sender, RoutedEventArgs e)
         {
             object selectedRaw = notesLB.SelectedItem;
@@ -268,6 +273,20 @@ namespace yNotes
 
             noteTB.Text = "";
         }
+        #endregion
+
+        private void copyB_Click(object sender, RoutedEventArgs e)
+        {
+            object selectedRaw = notesLB.SelectedItem;
+            selectedBlock = selectedRaw as TextBlock;
+
+            if (selectedBlock != null)
+            {
+                var dataPackage = new DataPackage();
+                dataPackage.SetText(selectedBlock.Text);
+                Clipboard.SetContent(dataPackage);
+            }
+        }
 
         private void ImportABB_Click(object sender, RoutedEventArgs e)
         {
@@ -304,5 +323,160 @@ namespace yNotes
 
             _ = noteLengthPR.Width;
         }
+
+        /*
+        #region Register for printing
+
+        private PrintManager printMan;
+        private PrintDocument printDoc;
+        private IPrintDocumentSource printDocSource;
+        private TextBlock textBlock;
+        private Canvas canvas;
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // Register for PrintTaskRequested event
+            printMan = PrintManager.GetForCurrentView();
+            printMan.PrintTaskRequested += PrintTaskRequested;
+
+            // Build a PrintDocument and register for callbacks
+            printDoc = new PrintDocument();
+            printDocSource = printDoc.DocumentSource;
+            printDoc.Paginate += Paginate;
+            printDoc.GetPreviewPage += GetPreviewPage;
+            printDoc.AddPages += AddPages;
+        }
+
+        #endregion
+
+        #region Showing the print dialog
+
+        private async void PrintButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (PrintManager.IsSupported())
+            {
+                try
+                {
+                    // Show print UI
+                    await PrintManager.ShowPrintUIAsync();
+                }
+                catch
+                {
+                    // Printing cannot proceed at this time
+                    ContentDialog noPrintingDialog = new ContentDialog()
+                    {
+                        Title = "Printing error",
+                        Content = "\nSorry, printing can' t proceed at this time.",
+                        PrimaryButtonText = "OK"
+                    };
+                    await noPrintingDialog.ShowAsync();
+                }
+            }
+            else
+            {
+                // Printing is not supported on this device
+                ContentDialog noPrintingDialog = new ContentDialog()
+                {
+                    Title = "Printing not supported",
+                    Content = "\nSorry, printing is not supported on this device.",
+                    PrimaryButtonText = "OK"
+                };
+                await noPrintingDialog.ShowAsync();
+            }
+        }
+
+        private void PrintTaskRequested(PrintManager sender, PrintTaskRequestedEventArgs args)
+        {
+            // Create the PrintTask.
+            // Defines the title and delegate for PrintTaskSourceRequested
+            var printTask = args.Request.CreatePrintTask("Print", PrintTaskSourceRequrested);
+
+            // Handle PrintTask.Completed to catch failed print jobs
+            printTask.Completed += PrintTaskCompleted;
+        }
+
+        private void PrintTaskSourceRequrested(PrintTaskSourceRequestedArgs args)
+        {
+            // Set the document source.
+            args.SetSource(printDocSource);
+        }
+
+        #endregion
+
+        #region Print preview
+
+        private void Paginate(object sender, PaginateEventArgs e)
+        {
+            // As I only want to print one Rectangle, so I set the count to 1
+            printDoc.SetPreviewPageCount(1, PreviewPageCountType.Final);
+        }
+
+        private void GetPreviewPage(object sender, GetPreviewPageEventArgs e)
+        {
+            textBlock = new TextBlock
+            {
+                Foreground = new SolidColorBrush(Color.FromArgb(255, 0, 0, 0)),
+                FontFamily = FontFamily.XamlAutoFontFamily,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                VerticalAlignment = VerticalAlignment.Center
+            };
+            string text = "";
+
+            foreach (object item in notesLB.Items)
+            {
+                TextBlock itemAsTB = item as TextBlock;
+                if (itemAsTB != null)
+                {
+                    text += itemAsTB.Text + "\n";
+                }
+            }
+
+            textBlock.Text = text;
+
+            canvas = new Canvas();
+            canvas.HorizontalAlignment = HorizontalAlignment.Stretch;
+            canvas.VerticalAlignment = VerticalAlignment.Stretch;
+            canvas.Children.Add(textBlock);
+
+            // Provide a UIElement as the print preview.
+            printDoc.SetPreviewPage(e.PageNumber, canvas);
+        }
+
+        #endregion
+
+        #region Add pages to send to the printer
+
+        private void AddPages(object sender, AddPagesEventArgs e)
+        {
+            printDoc.AddPage(textBlock);
+
+            // Indicate that all of the print pages have been provided
+            printDoc.AddPagesComplete();
+        }
+
+        #endregion
+
+        #region Print task completed
+
+        private async void PrintTaskCompleted(PrintTask sender, PrintTaskCompletedEventArgs args)
+        {
+            // Notify the user when the print operation fails.
+            if (args.Completion == PrintTaskCompletion.Failed)
+            {
+                await CoreApplication.MainView.CoreWindow.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, async () =>
+                {
+                    ContentDialog noPrintingDialog = new ContentDialog()
+                    {
+                        Title = "Printing error",
+                        Content = "\nSorry, failed to print.",
+                        PrimaryButtonText = "OK"
+                    };
+                    await noPrintingDialog.ShowAsync();
+                });
+            }
+        }
+
+        #endregion
+        */
     }
 }
